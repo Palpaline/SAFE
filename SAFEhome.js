fd7c7225f6eb5e517bc4c0f483f384875359eab5
AmandineLib <amand.libert@gmail.com>
2023-10-20 16:57:07 +0200

    test pop up

--- SAFEhome.js
+++ SAFEhome.js
@@ -1,24 +1,42 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
     if (sessionStorage.getItem('advertOnce') !== 'true') {
-    //sessionStorage.setItem('advertOnce','true');
-      $('.box').show();
-    }else{
-      $('.box').hide();
+        //sessionStorage.setItem('advertOnce','true');
+        $('.box').show();
+    } else {
+        $('.box').hide();
     }
-     
-    $('#refresh-page').on('click',function(){
-    $('.box').hide();
-    sessionStorage.setItem('advertOnce','true');
-    });
-      
-    $('#reset-session').on('click',function(){
-    $('.box').show();
-    sessionStorage.setItem('advertOnce','');
-    });
-     
+
+    $('#refresh-page').on('click', function () {
+        $('.box').hide();
+        sessionStorage.setItem('advertOnce', 'true');
     });
 
-    // pop up 
+    $('#reset-session').on('click', function () {
+        $('.box').show();
+        sessionStorage.setItem('advertOnce', '');
+    });
 
-    
+});
+
+// pop up 
+
+const openModalButtons = document.querySelectorAll('.open-modal'),
+      modal = document.querySelector('.modal'),
+      closeModalButtons = document.querySelectorAll('.close-modal');
+
+openModalButtons.forEach(openBtn => {
+  openBtn.addEventListener('click', openModal)
+});
+
+closeModalButtons.forEach(closeBtn => {
+  closeBtn.addEventListener('click', closeModal)
+});
+
+function openModal() {
+  modal.classList.add('visible');
+}
+
+function closeModal() {
+  modal.classList.remove('visible');
+}
